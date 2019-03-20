2604cde627fc7e095f92f6361c0797264b02bc92
Sam <samgenieboi@gmail.com>
2019-03-20 15:01:23 +1000

    Update

--- server/main.js
+++ server/main.js
@@ -2,10 +2,16 @@ var WebSocket = require("ws");
 
 var wss = new WebSocket.Server({port: 8080});
 
+var game = {
+    time: 0,
+    last: 0,
+    users: []
+};
+
 wss.on("connection", ws => {
     ws.on("message", message => {
         try {
-            var data = JSON.stringify(message);
+            var data = JSON.parse(message);
         } catch {
             return console.log;
         }
@@ -14,21 +20,59 @@ wss.on("connection", ws => {
             case "CONNECT":
                 console.log("CONNECT", data);
 
-                send
+                game.users.push({
+                    // x: 0,
+                    // y: 0,
+                    // width: 100,
+                    // height: 100,
+                    // radius: 50,
+                    // stats: {
+                    //     kills: 0,
+                    //     deaths: 0
+                    // },
+                    // speed: 0.2,
+                    // friction: 1
+
+                    id: game.users.length,
+                    x: 0,
+                    y: 0,
+
+                    stats: {
+                        kills: 0,
+                        deaths: 0
+                    }
+                });
+
+                send(ws, {
+                    type: "CONNECTED",
+                    user_id: game.users.length - 1,
+                    users: game.users
+                });
+
+                ws.user_id = game.users.length - 1;
 
                 break;
             case "UPDATE":
-                console.log("UPDATE", data);
+                // console.log("UPDATE", data);
 
-                // update
+                var id = ws.user_id;
+
+                if (id === undefined) return;
+                if (game.users[id] === undefined) return;
+
+                game.users[id].x = data.x;
+                game.users[id].y = data.y;
+                game.users[id].velocity = data.velocity;
+                game.users[id].halfwidth = data.halfwidth;
+                game.users[id].halfheight = data.halfheight;
         }
-
-        console.log(message);
     });
 
     ws.on("close", () => {
-        if (ws.user_id) {
+        if (ws.user_id !== undefined) {
+            // game.users.splice(ws.user_id);
 
+            game.users[ws.user_id] = {};
         }
     });
 });
@@ -37,29 +81,23 @@ function send(ws, message) {
     ws.send(JSON.stringify(message));
 }
 
-function broadcast(message) {
+function update() {
+    game.time = Date.now();
+    var delta = game.time - game.last;
+
     wss.clients.forEach(ws => {
-        if (ws.id) {
-            send(ws, message);
+        if (ws.user_id !== undefined) {
+            if (ws.readyState === 1) {
+                send(ws, {
+                    type: "UPDATE",
+                    user_id: ws.user_id,
+                    users: game.users
+                });
+            }
         }
     });
-}
 
-var time = Date.now();
-var last = time;
-var game = {
-    time: 0,
-    last: 0,
-    players: {}
-}
-
-function update() {
-    time = Date.now();
-    var delta = time - last;
-
-    // game
-
-    last = Date.now();
+    game.last = Date.now();
 }
 
 setInterval(update, 30);
